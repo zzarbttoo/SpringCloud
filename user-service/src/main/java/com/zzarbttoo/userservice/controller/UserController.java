e17f7f61c66b0c8341d38c52f5546cb499528ca1
zzarbttoo <qodbwls70@naver.com>
2021-09-09 22:30:47 +0900

    env : configuration setting(server restart)

--- user-service/src/main/java/com/zzarbttoo/userservice/controller/UserController.java
+++ user-service/src/main/java/com/zzarbttoo/userservice/controller/UserController.java
@@ -40,7 +40,11 @@ public class UserController {
     @GetMapping("/health_check")
     public String status(){
         //포트번호를 출력할 수 있도록 변경
-        return String.format("It's Working in User Service on Port %s", env.getProperty("local.server.port"));
+        return String.format("It's Working in User Service"
+        + ", port(local.server.port) = " + env.getProperty("local.server.port")
+        + ", port(server.port) = " + env.getProperty("server.port")
+        + ", token secret = " + env.getProperty("token.secret")
+        + ", token expiration time = " + env.getProperty("token.expiration_time"));
     }
 
     @GetMapping("/welcome")
